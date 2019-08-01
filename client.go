e71b89ee0c2bb72185d9ac1c4aae4dbaff46bb04
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2019-08-01 16:30:31 +0200

    sort out time calc
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- client.go
+++ client.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"sync"
 	"time"
-)
 
-import (
 	"github.com/GaryBoone/GoStats/stats"
+
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+type SubTimes map[string][]float64
+
 // Client - represents mqtt client
 type Client struct {
-	ID         int
+	ID         string
 	BrokerURL  string
 	BrokerUser string
 	BrokerPass string
@@ -25,11 +28,11 @@ type Client struct {
 }
 
 // RunPublisher - runs publisher
-func (c *Client) RunPublisher(res chan *RunResults, donePub chan bool) {
+func (c *Client) RunPublisher(res chan *RunResults) {
 	newMsgs := make(chan *Message)
 	pubMsgs := make(chan *Message)
 	doneGen := make(chan bool)
-
+	donePub := make(chan bool)
 	runResults := new(RunResults)
 
 	started := time.Now()
@@ -38,18 +41,19 @@ func (c *Client) RunPublisher(res chan *RunResults, donePub chan bool) {
 	// start publisher
 	go c.pubMessages(newMsgs, pubMsgs, doneGen, donePub)
 
-	runResults.ID = c.ID
 	times := []float64{}
 	for {
 		select {
 		case m := <-pubMsgs:
+			cid := m.ID
 			if m.Error {
 				log.Printf("CLIENT %v ERROR publishing message: %v: at %v\n", c.ID, m.Topic, m.Sent.Unix())
 				runResults.Failures++
 			} else {
-				// log.Printf("Message published: %v: sent: %v delivered: %v flight time: %v\n", m.Topic, m.Sent, m.Delivered, m.Delivered.Sub(m.Sent))
+				log.Printf("Message published: %v: sent: %v delivered: %v flight time: %v\n", m.Topic, m.Sent, m.Delivered, m.Delivered.Sub(m.Sent))
 				runResults.Successes++
-				times = append(times, m.Delivered.Sub(m.Sent).Seconds()*1000) // in milliseconds
+				runResults.ID = cid
+				times = append(times, float64(m.Delivered.Sub(m.Sent).Nanoseconds()/1000)) // in microseconds
 			}
 		case <-donePub:
 			// calculate results
@@ -69,42 +73,22 @@ func (c *Client) RunPublisher(res chan *RunResults, donePub chan bool) {
 }
 
 // RunSubscriber - runs a subscriber
-func (c *Client) RunSubscriber(res chan *RunResults, donePub chan bool) {
-	doneSub := make(chan bool)
-	runResults := new(RunResults)
-
-	started := time.Now()
+func (c *Client) RunSubscriber(wg *sync.WaitGroup, subTimes *SubTimes) {
+	defer wg.Done()
 	// start generator
 	// start subscriber
-	go c.subscribe(doneSub, doneSub)
 
-	runResults.ID = c.ID
-	times := []float64{}
-	for {
-		select {
-		case <-doneSub:
-			// calculate results
-			duration := time.Now().Sub(started)
-			runResults.MsgTimeMin = stats.StatsMin(times)
-			runResults.MsgTimeMax = stats.StatsMax(times)
-			runResults.MsgTimeMean = stats.StatsMean(times)
-			runResults.MsgTimeStd = stats.StatsSampleStandardDeviation(times)
-			runResults.RunTime = duration.Seconds()
-			runResults.MsgsPerSec = float64(runResults.Successes) / duration.Seconds()
+	c.subscribe(subTimes)
 
-			// report results and exit
-			res <- runResults
-			return
-		}
-	}
 }
 
 func (c *Client) genMessages(ch chan *Message, done chan bool) {
 	for i := 0; i < c.MsgCount; i++ {
+		msgPayload := MessagePayload{Payload: make([]byte, c.MsgSize)}
 		ch <- &Message{
 			Topic:   c.MsgTopic,
 			QoS:     c.MsgQoS,
-			Payload: make([]byte, c.MsgSize),
+			Payload: msgPayload,
 		}
 	}
 	done <- true
@@ -112,26 +96,25 @@ func (c *Client) genMessages(ch chan *Message, done chan bool) {
 	return
 }
 
-func (c *Client) subscribe(doneSub chan bool, donePub chan bool) {
+func (c *Client) subscribe(subTimes *SubTimes) {
+
+	clientID := fmt.Sprintf("sub-%v-%v", time.Now().Format(time.RFC3339Nano), c.ID)
+	c.ID = clientID
+
 	onConnected := func(client mqtt.Client) {
 		if !c.Quiet {
-			log.Printf("CLIENT %v is connected to the broker %v\n", c.ID, c.BrokerURL)
+			log.Printf("CLIENT %v is connected to the broker %v\n", clientID, c.BrokerURL)
 		}
-		select {
-		case <-donePub:
-			doneSub <- true
-		}
-
 	}
 
 	opts := mqtt.NewClientOptions().
 		AddBroker(c.BrokerURL).
-		SetClientID(fmt.Sprintf("mqtt-benchmark-%v-%v", time.Now().Format(time.RFC3339Nano), c.ID)).
+		SetClientID(clientID).
 		SetCleanSession(true).
 		SetAutoReconnect(true).
 		SetOnConnectHandler(onConnected).
 		SetConnectionLostHandler(func(client mqtt.Client, reason error) {
-			log.Printf("CLIENT %v lost connection to the broker: %v. Will reconnect...\n", c.ID, reason.Error())
+			log.Printf("CLIENT %v lost connection to the broker: %v. Will reconnect...\n", clientID, reason.Error())
 		})
 	if c.BrokerUser != "" && c.BrokerPass != "" {
 		opts.SetUsername(c.BrokerUser)
@@ -142,32 +125,50 @@ func (c *Client) subscribe(doneSub chan bool, donePub chan bool) {
 	token.Wait()
 
 	if token.Error() != nil {
-		log.Printf("CLIENT %v had error connecting to the broker: %v\n", c.ID, token.Error())
+		log.Printf("CLIENT %v had error connecting to the broker: %v\n", clientID, token.Error())
 	}
 
-	token = client.Subscribe(c.MsgTopic, 0, func(c mqtt.Client, msg mqtt.Message) {
-		log.Printf("CLIENT received message on topic: %v\n", msg.Topic())
+	token = client.Subscribe(c.MsgTopic, 0, func(cl mqtt.Client, msg mqtt.Message) {
+
+		mp := MessagePayload{}
+		err := json.Unmarshal(msg.Payload(), &mp)
+		if err != nil {
+			log.Printf("CLIENT %s failed to decode message", clientID)
+		}
+		log.Printf("CLIENT %s received message on topic: %v - %s\n", clientID, msg.Topic(), mp.ID)
+		// if (*subTimes)[mp.ID] == nil {
+		// 	(*subTimes)[mp.ID] = []float64{}
+		// }
+		// (*subTimes)[mp.ID] = append((*subTimes)[mp.ID], float64(time.Now().Sub(mp.Sent).Nanoseconds()/1000))
 	})
 
 	token.Wait()
-	doneSub <- true
+	fmt.Println("exit subscribing")
 
 }
 
-func (c *Client) pubMessages(in, out chan *Message, doneGen, donePub chan bool) {
+func (c *Client) pubMessages(in, out chan *Message, doneGen chan bool, donePub chan bool) {
+
+	clientID := fmt.Sprintf("pub-%v-%v", time.Now().Format(time.RFC3339Nano), c.ID)
+	c.ID = clientID
 	onConnected := func(client mqtt.Client) {
 		if !c.Quiet {
-			log.Printf("CLIENT %v is connected to the broker %v\n", c.ID, c.BrokerURL)
+			log.Printf("CLIENT %v is connected to the broker %v\n", clientID, c.BrokerURL)
 		}
 		ctr := 0
 		for {
 			select {
 			case m := <-in:
 				m.Sent = time.Now()
-				token := client.Publish(m.Topic, m.QoS, false, m.Payload)
+				m.ID = clientID
+				m.Payload.ID = clientID
+				m.Payload.Sent = m.Sent
+
+				pload, _ := json.Marshal(m.Payload)
+				token := client.Publish(m.Topic, m.QoS, false, pload)
 				token.Wait()
 				if token.Error() != nil {
-					log.Printf("CLIENT %v Error sending message: %v\n", c.ID, token.Error())
+					log.Printf("CLIENT %v Error sending message: %v\n", clientID, token.Error())
 					m.Error = true
 				} else {
 					m.Delivered = time.Now()
@@ -177,14 +178,14 @@ func (c *Client) pubMessages(in, out chan *Message, doneGen, donePub chan bool) 
 
 				if ctr > 0 && ctr%100 == 0 {
 					if !c.Quiet {
-						log.Printf("CLIENT %v published %v messages and keeps publishing...\n", c.ID, ctr)
+						log.Printf("CLIENT %v published %v messages and keeps publishing...\n", clientID, ctr)
 					}
 				}
 				ctr++
 			case <-doneGen:
 				donePub <- true
 				if !c.Quiet {
-					log.Printf("CLIENT %v is done publishing\n", c.ID)
+					log.Printf("CLIENT %v is done publishing\n", clientID)
 				}
 				return
 			}
@@ -193,12 +194,13 @@ func (c *Client) pubMessages(in, out chan *Message, doneGen, donePub chan bool) 
 
 	opts := mqtt.NewClientOptions().
 		AddBroker(c.BrokerURL).
-		SetClientID(fmt.Sprintf("mqtt-benchmark-%v-%v", time.Now().Format(time.RFC3339Nano), c.ID)).
+		//SetClientID(fmt.Sprintf("mqtt-benchmark-%v-%v", time.Now().Format(time.RFC3339Nano), c.ID)).
+		SetClientID(clientID).
 		SetCleanSession(true).
 		SetAutoReconnect(true).
 		SetOnConnectHandler(onConnected).
 		SetConnectionLostHandler(func(client mqtt.Client, reason error) {
-			log.Printf("CLIENT %v lost connection to the broker: %v. Will reconnect...\n", c.ID, reason.Error())
+			log.Printf("CLIENT %v lost connection to the broker: %v. Will reconnect...\n", clientID, reason.Error())
 		})
 	if c.BrokerUser != "" && c.BrokerPass != "" {
 		opts.SetUsername(c.BrokerUser)
--- main.go
+++ main.go
@@ -8,16 +8,24 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/GaryBoone/GoStats/stats"
 )
 
 // Message describes a message
+type MessagePayload struct {
+	ID      string
+	Sent    time.Time
+	Payload interface{}
+}
 type Message struct {
+	ID             string
 	Topic          string
 	QoS            byte
-	Payload        interface{}
+	Payload        MessagePayload
 	Sent           time.Time
 	Delivered      time.Time
 	DeliveredToSub time.Time
@@ -26,30 +34,41 @@ type Message struct {
 
 // RunResults describes results of a single client / run
 type RunResults struct {
-	ID          int     `json:"id"`
-	Successes   int64   `json:"successes"`
-	Failures    int64   `json:"failures"`
+	ID        string `json:"id"`
+	Successes int64  `json:"successes"`
+	Failures  int64  `json:"failures"`
+
 	RunTime     float64 `json:"run_time"`
 	MsgTimeMin  float64 `json:"msg_time_min"`
 	MsgTimeMax  float64 `json:"msg_time_max"`
 	MsgTimeMean float64 `json:"msg_time_mean"`
 	MsgTimeStd  float64 `json:"msg_time_std"`
-	MsgsPerSec  float64 `json:"msgs_per_sec"`
+
+	MsgDelTimeMin  float64 `json:"msg_del_time_min"`
+	MsgDelTimeMax  float64 `json:"msg_del_time_max"`
+	MsgDelTimeMean float64 `json:"msg_del_time_mean"`
+	MsgDelTimeStd  float64 `json:"msg_del_time_std"`
+
+	MsgsPerSec float64 `json:"msgs_per_sec"`
 }
 
 // TotalResults describes results of all clients / runs
 type TotalResults struct {
-	Ratio           float64 `json:"ratio"`
-	Successes       int64   `json:"successes"`
-	Failures        int64   `json:"failures"`
-	TotalRunTime    float64 `json:"total_run_time"`
-	AvgRunTime      float64 `json:"avg_run_time"`
-	MsgTimeMin      float64 `json:"msg_time_min"`
-	MsgTimeMax      float64 `json:"msg_time_max"`
-	MsgTimeMeanAvg  float64 `json:"msg_time_mean_avg"`
-	MsgTimeMeanStd  float64 `json:"msg_time_mean_std"`
-	TotalMsgsPerSec float64 `json:"total_msgs_per_sec"`
-	AvgMsgsPerSec   float64 `json:"avg_msgs_per_sec"`
+	Ratio             float64 `json:"ratio"`
+	Successes         int64   `json:"successes"`
+	Failures          int64   `json:"failures"`
+	TotalRunTime      float64 `json:"total_run_time"`
+	AvgRunTime        float64 `json:"avg_run_time"`
+	MsgTimeMin        float64 `json:"msg_time_min"`
+	MsgTimeMax        float64 `json:"msg_time_max"`
+	MsgDelTimeMin     float64 `json:"msg_del_time_min"`
+	MsgDelTimeMax     float64 `json:"msg_del_time_max"`
+	MsgTimeMeanAvg    float64 `json:"msg_time_mean_avg"`
+	MsgTimeMeanStd    float64 `json:"msg_time_mean_std"`
+	MsgDelTimeMeanAvg float64 `json:"msg_del_time_mean_avg"`
+	MsgDelTimeMeanStd float64 `json:"msg_del_time_mean_std"`
+	TotalMsgsPerSec   float64 `json:"total_msgs_per_sec"`
+	AvgMsgsPerSec     float64 `json:"avg_msgs_per_sec"`
 }
 
 // JSONResults are used to export results as a JSON document
@@ -68,18 +87,17 @@ type Connection struct {
 func main() {
 
 	var (
-		broker = flag.String("broker", "tcp://142.93.118.47:1883", "MQTT broker endpoint as scheme://host:port")
-		//topic    = flag.String("topic", "/test", "MQTT topic for outgoing messages")
-		//username = flag.String("username", "", "MQTT username (empty if auth disabled)")
-		//password = flag.String("password", "", "MQTT password (empty if auth disabled)")
+		broker = flag.String("broker", "tcp://localhost:1883", "MQTT broker endpoint as scheme://host:port")
 		qos    = flag.Int("qos", 1, "QoS for published messages")
 		size   = flag.Int("size", 100, "Size of the messages payload (bytes)")
-		count  = flag.Int("count", 100, "Number of messages to send per client")
+		count  = flag.Int("count", 10, "Number of messages to send per client")
 		pubs   = flag.Int("pubs", 10, "Number of clients to start")
 		subs   = flag.Int("subs", 10, "Number of clients to start")
 		format = flag.String("format", "text", "Output format: text|json")
 		quiet  = flag.Bool("quiet", false, "Suppress logs while running")
 	)
+	var wg sync.WaitGroup
+	subTimes := make(SubTimes)
 
 	flag.Parse()
 	if *pubs < 1 && *subs < 1 {
@@ -102,7 +120,7 @@ func main() {
 	json.Unmarshal([]byte(byteValue), &connections)
 
 	resCh := make(chan *RunResults)
-	donePub := make(chan bool)
+
 	start := time.Now()
 	n := len(connections)
 	for i := 0; i < *subs; i++ {
@@ -113,7 +131,7 @@ func main() {
 		con := connections[i/n]
 
 		c := &Client{
-			ID:         i,
+			ID:         strconv.Itoa(i),
 			BrokerURL:  *broker,
 			BrokerUser: con.ThingID,
 			BrokerPass: con.ThingKey,
@@ -123,16 +141,18 @@ func main() {
 			MsgQoS:     byte(*qos),
 			Quiet:      *quiet,
 		}
-		go c.RunSubscriber(resCh, donePub)
+		wg.Add(1)
+		go c.RunSubscriber(&wg, &subTimes)
 	}
 
+	wg.Wait()
 	for i := 0; i < *pubs; i++ {
 		if !*quiet {
 			log.Println("Starting pub client ", i)
 		}
 		con := connections[i/n]
 		c := &Client{
-			ID:         i,
+			ID:         strconv.Itoa(i),
 			BrokerURL:  *broker,
 			BrokerUser: con.ThingID,
 			BrokerPass: con.ThingKey,
@@ -142,31 +162,46 @@ func main() {
 			MsgQoS:     byte(*qos),
 			Quiet:      *quiet,
 		}
-		go c.RunPublisher(resCh, donePub)
+		go c.RunPublisher(resCh)
 	}
 
 	// collect the results
-	results := make([]*RunResults, *subs+*pubs)
-	for i := 0; i < *subs+*pubs; i++ {
+	fmt.Printf("collecting results")
+	results := make([]*RunResults, *pubs)
+	for i := 0; i < *pubs; i++ {
 		results[i] = <-resCh
 	}
+	fmt.Println("processing results")
 	totalTime := time.Now().Sub(start)
-	totals := calculateTotalResults(results, totalTime)
+	totals := calculateTotalResults(results, totalTime, &subTimes)
 
 	// print stats
 	printResults(results, totals, *format)
 }
-func calculateTotalResults(results []*RunResults, totalTime time.Duration) *TotalResults {
+func calculateTotalResults(results []*RunResults, totalTime time.Duration, subTimes *SubTimes) *TotalResults {
 	totals := new(TotalResults)
 	totals.TotalRunTime = totalTime.Seconds()
-
+	var subTimeRunResults RunResults
 	msgTimeMeans := make([]float64, len(results))
+	msgTimeMeansDelivered := make([]float64, len(results))
 	msgsPerSecs := make([]float64, len(results))
 	runTimes := make([]float64, len(results))
 	bws := make([]float64, len(results))
 
 	totals.MsgTimeMin = results[0].MsgTimeMin
 	for i, res := range results {
+
+		times := (*subTimes)[res.ID]
+		subTimeRunResults.MsgTimeMin = stats.StatsMin(times)
+		subTimeRunResults.MsgTimeMax = stats.StatsMax(times)
+		subTimeRunResults.MsgTimeMean = stats.StatsMean(times)
+		subTimeRunResults.MsgTimeStd = stats.StatsSampleStandardDeviation(times)
+
+		res.MsgDelTimeMin = subTimeRunResults.MsgTimeMin
+		res.MsgDelTimeMax = subTimeRunResults.MsgTimeMax
+		res.MsgDelTimeMean = subTimeRunResults.MsgTimeMean
+		res.MsgDelTimeStd = subTimeRunResults.MsgTimeStd
+
 		totals.Successes += res.Successes
 		totals.Failures += res.Failures
 		totals.TotalMsgsPerSec += res.MsgsPerSec
@@ -179,6 +214,15 @@ func calculateTotalResults(results []*RunResults, totalTime time.Duration) *Tota
 			totals.MsgTimeMax = res.MsgTimeMax
 		}
 
+		if subTimeRunResults.MsgTimeMin < totals.MsgDelTimeMin {
+			totals.MsgDelTimeMin = subTimeRunResults.MsgTimeMin
+		}
+
+		if subTimeRunResults.MsgTimeMax > totals.MsgDelTimeMax {
+			totals.MsgDelTimeMax = subTimeRunResults.MsgTimeMax
+		}
+
+		msgTimeMeansDelivered[i] = subTimeRunResults.MsgTimeMean
 		msgTimeMeans[i] = res.MsgTimeMean
 		msgsPerSecs[i] = res.MsgsPerSec
 		runTimes[i] = res.RunTime
@@ -187,6 +231,8 @@ func calculateTotalResults(results []*RunResults, totalTime time.Duration) *Tota
 	totals.Ratio = float64(totals.Successes) / float64(totals.Successes+totals.Failures)
 	totals.AvgMsgsPerSec = stats.StatsMean(msgsPerSecs)
 	totals.AvgRunTime = stats.StatsMean(runTimes)
+	totals.MsgDelTimeMeanAvg = stats.StatsMean(msgTimeMeansDelivered)
+	totals.MsgDelTimeMeanStd = stats.StatsSampleStandardDeviation(msgTimeMeansDelivered)
 	totals.MsgTimeMeanAvg = stats.StatsMean(msgTimeMeans)
 	totals.MsgTimeMeanStd = stats.StatsSampleStandardDeviation(msgTimeMeans)
 
@@ -207,23 +253,34 @@ func printResults(results []*RunResults, totals *TotalResults, format string) {
 		fmt.Println(string(out.Bytes()))
 	default:
 		for _, res := range results {
-			fmt.Printf("======= CLIENT %d =======\n", res.ID)
+			fmt.Printf("======= CLIENT %s =======\n", res.ID)
 			fmt.Printf("Ratio:               %.3f (%d/%d)\n", float64(res.Successes)/float64(res.Successes+res.Failures), res.Successes, res.Successes+res.Failures)
 			fmt.Printf("Runtime (s):         %.3f\n", res.RunTime)
-			fmt.Printf("Msg time min (ms):   %.3f\n", res.MsgTimeMin)
-			fmt.Printf("Msg time max (ms):   %.3f\n", res.MsgTimeMax)
-			fmt.Printf("Msg time mean (ms):  %.3f\n", res.MsgTimeMean)
-			fmt.Printf("Msg time std (ms):   %.3f\n", res.MsgTimeStd)
+			fmt.Printf("Msg time min (us):   %.3f\n", res.MsgTimeMin)
+			fmt.Printf("Msg time max (us):   %.3f\n", res.MsgTimeMax)
+			fmt.Printf("Msg time mean (us):  %.3f\n", res.MsgTimeMean)
+			fmt.Printf("Msg time std (us):   %.3f\n", res.MsgTimeStd)
+			fmt.Printf(" ======\n")
+			fmt.Printf("Msg del time max (us):   %.3f\n", res.MsgDelTimeMax)
+			fmt.Printf("Msg del time mean (us):  %.3f\n", res.MsgDelTimeMean)
+			fmt.Printf("Msg time std (us):   %.3f\n", res.MsgDelTimeStd)
+
 			fmt.Printf("Bandwidth (msg/sec): %.3f\n\n", res.MsgsPerSec)
 		}
 		fmt.Printf("========= TOTAL (%d) =========\n", len(results))
 		fmt.Printf("Total Ratio:                 %.3f (%d/%d)\n", totals.Ratio, totals.Successes, totals.Successes+totals.Failures)
 		fmt.Printf("Total Runtime (sec):         %.3f\n", totals.TotalRunTime)
 		fmt.Printf("Average Runtime (sec):       %.3f\n", totals.AvgRunTime)
-		fmt.Printf("Msg time min (ms):           %.3f\n", totals.MsgTimeMin)
-		fmt.Printf("Msg time max (ms):           %.3f\n", totals.MsgTimeMax)
-		fmt.Printf("Msg time mean mean (ms):     %.3f\n", totals.MsgTimeMeanAvg)
-		fmt.Printf("Msg time mean std (ms):      %.3f\n", totals.MsgTimeMeanStd)
+		fmt.Printf("Msg time min (us):           %.3f\n", totals.MsgTimeMin)
+		fmt.Printf("Msg time max (us):           %.3f\n", totals.MsgTimeMax)
+		fmt.Printf("Msg time mean mean (us):     %.3f\n", totals.MsgTimeMeanAvg)
+		fmt.Printf("Msg time mean std (us):      %.3f\n", totals.MsgTimeMeanStd)
+		fmt.Printf(" ======\n")
+		fmt.Printf("Msg del time min (us):           %.3f\n", totals.MsgDelTimeMin)
+		fmt.Printf("Msg del time max (us):           %.3f\n", totals.MsgDelTimeMax)
+		fmt.Printf("Msg del time mean mean (us):     %.3f\n", totals.MsgDelTimeMeanAvg)
+		fmt.Printf("Msg del time mean std (us):      %.3f\n", totals.MsgDelTimeMeanStd)
+
 		fmt.Printf("Average Bandwidth (msg/sec): %.3f\n", totals.AvgMsgsPerSec)
 		fmt.Printf("Total Bandwidth (msg/sec):   %.3f\n", totals.TotalMsgsPerSec)
 	}
@@ -231,5 +288,5 @@ func printResults(results []*RunResults, totals *TotalResults, format string) {
 }
 
 func getTestTopic(channelID string) string {
-	return "channels/" + channelID + "/message/test"
+	return "channels/" + channelID + "/messages/test"
 }
